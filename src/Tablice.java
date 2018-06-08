94180e0684180e8d81b926f405579127e9b7f3cf
Mikołaj Klonowski <nick-93@o2.pl>
2018-06-08 19:23:41 +0200

    Pierwsze zadanie o nr  16 poprawka2

--- src/Tablice.java
+++ src/Tablice.java
@@ -7,20 +7,15 @@ public class Tablice {
         for (int i = 0; i < 10; i++) {
             tablica[i] = rand.nextInt(10);
         }
-        int[] tablica2 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
-      //  int suma = 0;
-        int a=9;
+        int a = 9;
         for (int i = 0; i < 21; i++) {
-if (i<10){
-            System.out.print(tablica[i]+"   ");
-
-
-        } else if (i>10) {
-
-    System.out.print(tablica[a]+ " ");
-    a-=1;
-
+            if (i < 10) {
+                System.out.print(tablica[i] + "   ");
+            } else if (i > 10) {
+                System.out.print(tablica[a] + " ");
+                a -= 1;
             }
+        }
     }
-}}
+}
 
